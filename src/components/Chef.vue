45ae908b5a8cfd9290dd5b3e9e826eaf3e66fe7b
YoussefLMT <mcyoussef324@gmail.com>
2022-09-16 19:22:37 +0100

    add style for chef component

--- src/components/Chef.vue
+++ src/components/Chef.vue
@@ -16,5 +16,28 @@ export default {
 </script>
 
 <style scoped>
+.chef .content .box {
+    width: 300px;
+    margin: 15px;
+    border: 15px solid #fff;
+    box-shadow: 0 5px 35px rgba(0, 0, 0, 0.2);
+}
 
+.chef .content .box .imgbox {
+    position: relative;
+    width: 100%;
+    height: 250px;
+}
+
+.chef .content .box .imgbox img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
+.chef .content .box .text {
+    padding: 15px 0 5px;
+}
 </style>
