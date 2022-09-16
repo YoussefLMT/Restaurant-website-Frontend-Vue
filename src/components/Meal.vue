50e243a480ebd9b6bae0c32998c5dc20193ba84f
YoussefLMT <mcyoussef324@gmail.com>
2022-09-16 18:45:26 +0100

    add style for meal component

--- src/components/Meal.vue
+++ src/components/Meal.vue
@@ -15,5 +15,51 @@ export default {
 </script>
 
 <style scoped>
+.box{
+    width: 250px;
+    margin: 20px;
+    border: 15px solid #fff;
+    box-shadow: 0 5px 35px rgba(0,0,0,0.2);
+    border-radius: 15px;
+}
 
+.box .imgbox{
+    position: relative;
+    width: 100%;
+    height: 180px;
+}
+
+.imgbox img{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
+.box .text{
+    padding: 10px 0 0px;
+    text-align: center;
+}
+
+.box .text p{
+    color: #e74c3c;
+}
+
+.btn-order{
+    font-size: 15px;
+    color: #fff;
+    background: #e74c3c; 
+    display: inline-block; 
+    padding: 10px 30px; 
+    margin-top: 5px; 
+    text-transform: uppercase;
+    text-decoration: none;
+    letter-spacing: 2px;
+}
+
+.btn-order:hover{
+    background: hsl(6, 84%, 34%);  
+    border: none;
+}
 </style>
