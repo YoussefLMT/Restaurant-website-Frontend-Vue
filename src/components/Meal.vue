3618ac8b4d32ddb4cec56932a02d25b31edd955d
YoussefLMT <mcyoussef324@gmail.com>
2022-09-16 18:44:07 +0100

    add meal component

---
+++ src/components/Meal.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="box">
+    <div class="imgbox"> <img src="@/assets/pexels-ella-olsson-1640777.jpg" class="img_fluid"> </div>
+    <div class="text">
+        <h3>tt</h3>
+        <p>tt</p> <a href="#" name="order" class="btn-order">Order</a>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
