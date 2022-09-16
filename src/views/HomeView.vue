9506880773aa129786311a0532d0dd8748590224
YoussefLMT <mcyoussef324@gmail.com>
2022-09-16 18:30:05 +0100

    add about section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,6 +12,27 @@
         </div>
     </div>
 </section>
+
+<section class="about">
+    <div class="row">
+        <div class="col50">
+            <h1 class="title-txt">About <span>Us</span></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Eaque reprehenderit, aut repellat, dolorem illo, voluptatem 
+               minima sapiente praesentium earum possimus nemo debitis est sunt? 
+               Cumque minus sit quis aliquam veniam!<br> Lorem ipsum dolor sit amet, 
+               consectetur adipisicing elit. Assumenda, deserunt maxime consequuntur 
+               dolores quae unde vitae, incidunt quisquam quibusdam voluptates sed iure ad, 
+               fugit provident optio nisi praesentium et doloremque? 
+            </p>
+        </div>
+        <div class="col50">
+            <div class="imgbox"> 
+              <img src="images/pexels-ella-olsson-1640777.jpg"> 
+            </div>
+        </div>
+    </div>
+</section>
 </template>
 
 <script>
