9597c20c829b9b73bc79759c4c0b6d22a360f4ef
YoussefLMT <mcyoussef324@gmail.com>
2022-09-16 18:06:37 +0100

    add navbar to the home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="home">
-    
+    <img alt="Vue logo" src="../assets/logo.png">
+    <NavBar />
   </div>
 </template>
 
 <script>
+import NavBar from '@/components/NavBar.vue'
+
 export default {
+  name: 'HomeView',
+  components: {
+    NavBar
+  }
 }
 </script>
