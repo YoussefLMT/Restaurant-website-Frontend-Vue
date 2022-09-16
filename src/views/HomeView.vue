f486ffd25071b8a30e7dd7029d6ca5d64faf47e9
YoussefLMT <mcyoussef324@gmail.com>
2022-09-16 18:11:13 +0100

    add the main section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <NavBar />
-  </div>
+<NavBar />
+<section class="main">
+    <div class="main-content">
+        <div class="content">
+            <h1><span>Are</span> you hungry?</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Sed autem voluptate itaque fugit possimus veritatis,
+                totam voluptates. Possimus molestias quisquam unde
+            </p>
+            <a href="#" class="btn">Our Menu</a>
+        </div>
+    </div>
+</section>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    NavBar
-  }
+    name: 'HomeView',
+    components: {
+        NavBar
+    }
 }
 </script>
