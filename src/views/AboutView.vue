08f90eee2c353b5a07bcb06829936d6f696d1be6
YoussefLMT <mcyoussef324@gmail.com>
2022-09-17 18:47:51 +0100

    add about div and some style

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -13,6 +13,21 @@
         </div>
     </div>
 </section>
+
+<div class="about">
+<div class="row">
+    <div class="col50 animate__animated animate__backInLeft">
+        <h1 class="title-txt">About <span>Us</span></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reprehenderit, aut repellat, dolorem illo, voluptatem minima sapiente praesentium earum possimus nemo debitis est sunt? Cumque minus sit quis aliquam veniam!<br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, deserunt maxime consequuntur dolores quae unde vitae, incidunt quisquam quibusdam voluptates sed iure ad, fugit provident optio nisi praesentium et doloremque? </p>
+    </div>
+    <div class="col50 animate__animated animate__backInRight">
+        <div class="imgbox">
+            <img src="@/assets/pexels-ella-olsson-1640777.jpg">
+        </div>
+    </div>
+</div>
+</div>
+
 </template>
 
 <script>
@@ -31,7 +46,7 @@ export default {
     background-position: center;
     background-size: cover;
     background-repeat: no-repeat;
-    height: 50vh;
+    height: 70vh;
 }
 
 .main .main-content {
@@ -77,5 +92,36 @@ export default {
 .btn:hover {
     background: hsl(6, 84%, 34%);
     border: none;
+}
+
+.about{
+    padding: 100px;
+}
+
+.row{
+    position: relative;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+}
+
+.row .col50{
+    position: relative;
+    width: 48%;
+}
+
+.row .col50 .imgbox{
+    position: relative;
+    width: 100%;
+    height: 300px;
+}
+
+.row .col50 .imgbox img{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
 }
 </style>
