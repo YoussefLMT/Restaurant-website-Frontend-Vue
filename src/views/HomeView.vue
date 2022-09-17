38bd7d8553c17f0d7ea5cf6a63d17b1fee28697d
YoussefLMT <mcyoussef324@gmail.com>
2022-09-17 12:16:26 +0100

    remove about section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,26 +25,7 @@
     </div>
 </section>
 
-<section class="about">
-    <div class="row">
-        <div class="col50 animate__animated animate__backInLeft">
-            <h1 class="title-txt">About <span>Us</span></h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Eaque reprehenderit, aut repellat, dolorem illo, voluptatem
-                minima sapiente praesentium earum possimus nemo debitis est sunt?
-                Cumque minus sit quis aliquam veniam!<br> Lorem ipsum dolor sit amet,
-                consectetur adipisicing elit. Assumenda, deserunt maxime consequuntur
-                dolores quae unde vitae, incidunt quisquam quibusdam voluptates sed iure ad,
-                fugit provident optio nisi praesentium et doloremque?
-            </p>
-        </div>
-        <div class="col50 animate__animated animate__backInRight">
-            <div class="imgbox">
-                <img src="@/assets/pexels-ella-olsson-1640777.jpg">
-            </div>
-        </div>
-    </div>
-</section>
+
 
 <section class="menu">
     <div class="title">
