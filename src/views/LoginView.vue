d6515d7ba8e779240d1031fa254c65826fa1a24f
YoussefLMT <mcyoussef324@gmail.com>
2022-09-17 18:23:04 +0100

    add style for login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,20 +1,24 @@
 <template>
-<div class="auth-content">
-    <form>
-        <h2 class="form-title">Login</h2>
-        <div>
-            <label>Email</label>
-            <input type="email" class="text-input">
-        </div>
-        <div>
-            <label>Password</label>
-            <input type="password" class="text-input">
-        </div>
-        <div>
-            <button type="button" class="btn btn-big">Login</button>
-        </div>
-    </form>
-</div>
+<section class="main">
+    <div class="auth-content">
+        <form>
+            <h2 class="form-title">Login</h2>
+            <div>
+                <label>Email</label>
+                <input type="email" class="text-input">
+            </div>
+            <div>
+                <label>Password</label>
+                <input type="password" class="text-input">
+            </div>
+            <div>
+                <button type="button" class="btn btn-big">Login</button>
+            </div>
+            <p>Or <router-link to="#">Register</router-link>
+            </p>
+        </form>
+    </div>
+</section>
 </template>
 
 <script>
@@ -24,5 +28,70 @@ export default {
 </script>
 
 <style scoped>
+.main {
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("@/assets/pexels-ella-olsson-1640777.jpg");
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 100vh;
+}
 
+.auth-content {
+    width: 30%;
+    margin: 50px auto;
+    padding: 20px;
+    background: white;
+    border-radius: 5px;
+}
+
+.auth-content .form-title {
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+.auth-content form div {
+    margin-bottom: 10px;
+}
+
+.auth-content form p {
+    text-align: center;
+}
+
+.auth-content form p a {
+    text-decoration: underline;
+}
+
+.text-input {
+    padding: 10px 10px;
+    display: block;
+    width: 100%;
+    border-radius: 5px;
+    border: 1px solid #e0e0e0;
+    outline: none;
+    color: #444;
+    font-size: 18px;
+}
+
+.text-input:focus {
+    border: 3px solid #006669;
+}
+
+.btn {
+    padding: 10px 5px;
+    background: #3498db;
+    color: white;
+    border: 1px solid transparent;
+    border-radius: 5px;
+    font-size: 18px;
+    cursor: pointer;
+}
+
+.btn-big {
+    padding: 10px 10px;
+}
+
+.btn:hover {
+    color: white;
+    background: #005255;
+}
 </style>
