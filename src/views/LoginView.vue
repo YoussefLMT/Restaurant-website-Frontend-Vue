886eea0417d7e47f6ec879f1edadfadce49c1df5
YoussefLMT <mcyoussef324@gmail.com>
2022-09-17 18:16:53 +0100

    add login view and login form

---
+++ src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="auth-content">
+    <form>
+        <h2 class="form-title">Login</h2>
+        <div>
+            <label>Email</label>
+            <input type="email" class="text-input">
+        </div>
+        <div>
+            <label>Password</label>
+            <input type="password" class="text-input">
+        </div>
+        <div>
+            <button type="button" class="btn btn-big">Login</button>
+        </div>
+    </form>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
