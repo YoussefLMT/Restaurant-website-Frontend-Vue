f5e3df60fd01a3df366289037249ea4936f98d73
YoussefLMT <mcyoussef324@gmail.com>
2022-09-20 13:47:01 +0100

    add script

--- src/components/SidebarLink.vue
+++ src/components/SidebarLink.vue
@@ -9,6 +9,24 @@
 </router-link>
 </template>
 
+<script>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { collapsed } from './state'
+
+export default {
+  props: {
+    to: { type: String, required: true },
+    icon: { type: String, required: true }
+  },
+  setup(props) {
+    const route = useRoute()
+    const isActive = computed(() => route.path === props.to)
+    return { isActive, collapsed }
+  }
+}
+</script>
+
 <style scoped>
 .fade-enter-active,
 .fade-leave-active {
