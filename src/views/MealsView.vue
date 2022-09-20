501705eb7f03c73155ce1e0d3682526aa655680f
YoussefLMT <mcyoussef324@gmail.com>
2022-09-20 19:52:40 +0100

    add meals view

---
+++ src/views/MealsView.vue
@@ -0,0 +1,24 @@
+<template>
+<Sidebar />
+
+<div :style="{ 'margin-left': sidebarWidth }" class="meals">
+    <h1>Meals</h1>
+</div>
+</template>
+
+<script>
+import Sidebar from '@/components/SideBar.vue'
+import { sidebarWidth } from '@/components/sidebarState'
+export default {
+  components: { Sidebar },
+  setup() {
+    return { sidebarWidth }
+  }
+}
+</script>
+
+<style scoped>
+.meals{
+    padding: 20px
+}
+</style>
