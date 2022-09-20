b1d69f07fa2beade29b0b0d4e2833318b9d3ed3a
YoussefLMT <mcyoussef324@gmail.com>
2022-09-20 13:42:45 +0100

    add style for the sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -21,5 +21,45 @@ export default {
 </script>
 
 <style scoped>
+:root {
+    --sidebar-bg-color: #2f855a;
+    --sidebar-item-hover: #38a169;
+    --sidebar-item-active: #276749;
+}
 
+.sidebar {
+    color: white;
+    background-color: var(--sidebar-bg-color);
+    float: left;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    padding: 0.5em;
+
+    transition: 0.3s ease;
+
+    display: flex;
+    flex-direction: column;
+}
+
+.sidebar h1 {
+    height: 2.5em;
+}
+
+.collapse-icon {
+    position: absolute;
+    bottom: 0;
+    padding: 0.75em;
+
+    color: rgba(255, 255, 255, 0.7);
+
+    transition: 0.2s linear;
+}
+
+.rotate-180 {
+    transform: rotate(180deg);
+    transition: 0.2s linear;
+}
 </style>
