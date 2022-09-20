6054f392ba4484bff154bcb3d6c7847809fec6bc
YoussefLMT <mcyoussef324@gmail.com>
2022-09-20 13:41:42 +0100

    add sidebar component

---
+++ src/components/SideBar.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="sidebar" :style="{ width: sidebarWidth }">
+    <h1>
+        <span v-if="collapsed">
+            <div>V</div>
+            <div>S</div>
+        </span>
+        <span v-else>Vue Sidebar</span>
+    </h1>
+
+    <span class="collapse-icon" :class="{ 'rotate-180': collapsed }" @click="toggleSidebar">
+        <i class="fas fa-angle-double-left" />
+    </span>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
