007b947ebfd6248afa400038c56b107aa6f0393c
YoussefLMT <mcyoussef324@gmail.com>
2022-09-20 13:43:26 +0100

    add script for sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -15,8 +15,15 @@
 </template>
 
 <script>
-export default {
+import SidebarLink from './SidebarLink'
+import { collapsed, toggleSidebar, sidebarWidth } from './state'
 
+export default {
+  props: {},
+  components: { SidebarLink },
+  setup() {
+    return { collapsed, toggleSidebar, sidebarWidth }
+  }
 }
 </script>
 
