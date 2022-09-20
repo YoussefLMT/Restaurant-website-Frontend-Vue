234d18e463f28087270a0c6955ec8cc53537a912
YoussefLMT <mcyoussef324@gmail.com>
2022-09-20 13:46:24 +0100

    add sidebar link component

---
+++ src/components/SidebarLink.vue
@@ -0,0 +1,55 @@
+<template>
+<router-link :to="to" class="link" :class="{ active: isActive }">
+    <i class="icon" :class="icon" />
+    <transition name="fade">
+        <span v-if="!collapsed">
+            <slot />
+        </span>
+    </transition>
+</router-link>
+</template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.1s;
+}
+
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
+}
+
+.link {
+    display: flex;
+    align-items: center;
+
+    cursor: pointer;
+    position: relative;
+    font-weight: 400;
+    user-select: none;
+
+    margin: 0.1em 0;
+    padding: 0.6em;
+    border-radius: 0.25em;
+    height: 1.8em;
+
+    color: white;
+    text-decoration: none;
+}
+
+.link:hover {
+    background-color: var(--sidebar-item-hover);
+}
+
+.link.active {
+    background-color: var(--sidebar-item-active);
+}
+
+.link .icon {
+    flex-shrink: 0;
+    font-size: 25px;
+    width: 25px;
+    margin-right: 10px;
+}
+</style>
