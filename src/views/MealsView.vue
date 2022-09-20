237dde9dd64dd5701e43ede26ce1414bd97b666c
YoussefLMT <mcyoussef324@gmail.com>
2022-09-20 19:56:14 +0100

    add meals table

--- src/views/MealsView.vue
+++ src/views/MealsView.vue
@@ -3,22 +3,67 @@
 
 <div :style="{ 'margin-left': sidebarWidth }" class="meals">
     <h1>Meals</h1>
+
+    <div class="card me" style="width: 1200px;">
+        <div class="card-header">
+            Meals Managment
+        </div>
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">name</th>
+                        <th scope="col">price</th>
+                        <th scope="col">quantity</th>
+                        <th scope="col">description</th>
+                        <th scope="col">image</th>
+                        <th scope="col">actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td><img src="@/assets/pexels-pixabay-326279.jpg"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 </template>
 
 <script>
 import Sidebar from '@/components/SideBar.vue'
-import { sidebarWidth } from '@/components/sidebarState'
+import {
+    sidebarWidth
+} from '@/components/sidebarState'
 export default {
-  components: { Sidebar },
-  setup() {
-    return { sidebarWidth }
-  }
+    components: {
+        Sidebar
+    },
+    setup() {
+        return {
+            sidebarWidth
+        }
+    }
 }
 </script>
 
 <style scoped>
-.meals{
+.meals {
     padding: 20px
+}
+
+.me {
+    margin: 50px auto;
+}
+
+img {
+    width: 50px;
 }
 </style>
