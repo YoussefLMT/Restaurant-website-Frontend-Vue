fa161aa9a215e537a4264fe325f284f06041b2b5
YoussefLMT <mcyoussef324@gmail.com>
2022-09-21 13:11:03 +0100

    add update meal view

---
+++ src/views/UpdateMealView.vue
@@ -0,0 +1,36 @@
+<template>
+<Sidebar />
+
+<div :style="{ 'margin-left': sidebarWidth }" class="meals">
+    <h1>Update Meal</h1>
+
+    <div class="card me" style="width: 1200px;">
+        <div class="card-header">
+            Meals Managment
+        </div>
+        <div class="card-body">
+
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import menu from '../components/menu'
+import {
+    SidebarMenu
+} from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
+import axiosInstance from '../axios'
+import Swal from 'sweetalert2'
+
+export default {
+    components: {
+        SidebarMenu
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
