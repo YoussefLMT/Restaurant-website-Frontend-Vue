c4b4e264044345a1b3b151687aaa30c979e9fce1
YoussefLMT <mcyoussef324@gmail.com>
2022-09-21 12:27:46 +0100

    add meals state, getters, mutations, action

--- src/store/MealsModule.js
+++ src/store/MealsModule.js
@@ -1,13 +1,36 @@
 const MealsModule = {
     namespaced: true,
     state: {
-        
+        meals: [],
+        loading: false
+
     },
     getters: {
+        meals(state) {
+            return state.meals
+        },
     },
     mutations: {
+        getMeals(state, meals) {
+            state.meals = meals
+        },
+
+        setLoading(state, loading) {
+            state.loading = loading
+        },
     },
     actions: {
+        getMeals({ commit }) {
+            commit('setLoading', true)
+
+            axiosInstance.get('/meals')
+                .then(response => {
+                    commit('getMeals', response.data.meals)
+                    commit('setLoading', false)
+                })
+                .catch(error => console.log(error))
+        },
+
     },
 }
 
