11898443fd2e1aeb647a165869deeb0903200071
YoussefLMT <mcyoussef324@gmail.com>
2022-09-21 12:24:49 +0100

    add MealsModule

---
+++ src/store/MealsModule.js
@@ -0,0 +1,14 @@
+const MealsModule = {
+    namespaced: true,
+    state: {
+        
+    },
+    getters: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+}
+
+export default MealsModule
