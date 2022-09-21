3a18f039d438b4312dd1a0e9f2405fc78a8f571b
YoussefLMT <mcyoussef324@gmail.com>
2022-09-21 20:12:20 +0100

    add home meals state, getter, action and mutation

--- src/store/HomeMealsModule.js
+++ src/store/HomeMealsModule.js
@@ -1,15 +1,40 @@
+import axiosInstance from '../axios'
+
 const HomeMealsModule = {
     namespaced: true,
     state: {
-
+        home_meals: [],
+        loading: false
     },
     getters: {
+        homeMeals(state) {
+            return state.meals
+        },
 
+        loading(state) {
+            return state.loading
+        }
     },
     mutations: {
+        getHomeMeals(state, home_meals) {
+            state.home_meals = home_meals
+        },
 
+        setLoading(state, loading) {
+            state.loading = loading
+        },
     },
     actions: {
+        getHomeMeals({ commit }) {
+            commit('setLoading', true)
+
+            axiosInstance.get('/specific-meals')
+                .then(response => {
+                    commit('getHomeMeals', response.data.meals)
+                    commit('setLoading', false)
+                })
+                .catch(error => console.log(error))
+        },
 
     },
 }
