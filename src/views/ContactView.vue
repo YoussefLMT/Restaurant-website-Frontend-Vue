987e2111f64c543dc597f48b013bd00557b84cea
YoussefLMT <mcyoussef324@gmail.com>
2022-09-22 22:09:49 +0100

    some changes

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -6,23 +6,24 @@
 <div class="container">
     <div class="row">
         <div class="contact">
-                <h2 class="text-center mb-3">Contact us</h2>
-                <p>If you have any questions, do not hesitate to ask them.</p>
-                <i class="fa fa-map-marker w3-text-blue mb-2" style="width: 30px"></i>
-                Chicago, US<br>
-                <i class="fa fa-phone w3-text-blue mb-2" style="width: 30px"></i>
-                Phone: +00 151515<br><i class="fa fa-envelope w3-text-blue mb-3" style="width: 30px"> </i>
-                Email: mail@mail.com<br>
-                <form>
-                    <input type="text" name="current_user" value='<%= session.getAttribute( "Username" ) %>' style="display: none;">
-                    <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Email" required name="Email"></p>
-                    <p>
-                        <input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Message" required name="Message">
-                    </p>
-                    <p>
-                        <button class="w3-button w3-black w3-padding-large" type="button">SEND MESSAGE</button>
-                    </p>
-                </form>
+            <h2 class="text-center mb-3">Contact us</h2>
+            <p>If you have any questions, do not hesitate to ask them.</p>
+            <i class="fa fa-map-marker w3-text-blue mb-2" style="width: 30px"></i>
+            Chicago, US<br>
+            <i class="fa fa-phone w3-text-blue mb-2" style="width: 30px"></i>
+            Phone: +00 151515<br><i class="fa fa-envelope w3-text-blue mb-3" style="width: 30px"> </i>
+            Email: mail@mail.com<br>
+            <form>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="email">
+                </div>
+                <div class="mb-3">
+                    <label for="message" class="form-label">Message</label>
+                    <textarea class="form-control" aria-label="With textarea" id="message"></textarea>
+                </div>
+                <button class="btn" type="button">SEND MESSAGE</button>
+            </form>
         </div>
     </div>
 </div>
@@ -58,4 +59,19 @@ export default {
     box-shadow: rgba(0, 0, 0, 0.35) 0px 0px 15px 0px;
 }
 
+.btn {
+    font-size: 15px;
+    color: #fff;
+    background: #e74c3c;
+    display: inline-block;
+    padding: 10px 30px;
+    text-transform: uppercase;
+    text-decoration: none;
+    letter-spacing: 2px;
+}
+
+.btn:hover {
+    background: hsl(6, 84%, 34%);
+    border: none;
+}
 </style>
