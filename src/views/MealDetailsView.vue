e316952a53821835035a420f81846d2b7830e8f3
YoussefLMT <mcyoussef324@gmail.com>
2022-09-22 13:05:34 +0100

    add quantity buttons style

--- src/views/MealDetailsView.vue
+++ src/views/MealDetailsView.vue
@@ -94,5 +94,25 @@ p {
     font-weight: bold;
 }
 
+.quantity-toggle {
+    display: flex;
+}
 
+.quantity-toggle input {
+    border: 0;
+    border-top: 2px solid #ddd;
+    border-bottom: 2px solid #ddd;
+    width: 2.5rem;
+    text-align: center;
+    padding: 0 .5rem;
+}
+
+.quantity-toggle button {
+    border: 2px solid #ddd;
+    padding: .5rem;
+    background: #f5f5f5;
+    color: #888;
+    font-size: 1rem;
+    cursor: pointer;
+}
 </style>
