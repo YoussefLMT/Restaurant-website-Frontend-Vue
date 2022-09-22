d399824fae89f634bcaa9fab4f3a16b45f1fab32
YoussefLMT <mcyoussef324@gmail.com>
2022-09-22 12:17:22 +0100

    add UsersModule

---
+++ src/store/UsersModule.js
@@ -0,0 +1,19 @@
+import axiosInstance from '../axios'
+
+const UsersModule = {
+    namespaced: true,
+    state: {
+
+    },
+    getters: {
+
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    },
+}
+
+export default UsersModule
