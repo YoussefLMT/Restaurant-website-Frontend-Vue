1e0357ab8b6b4f14ce0c865e0800c82a0be59d48
YoussefLMT <mcyoussef324@gmail.com>
2022-09-23 20:56:15 +0100

    add cart state, getters, mutation and action

--- src/store/CartModule.js
+++ src/store/CartModule.js
@@ -1,16 +1,40 @@
+import axiosInstance from '../axios'
+
 const CartModule = {
     namespaced: true,
     state: {
-
+        cart_meals: [],
+        loading: false
     },
     getters: {
+        cartMeals(state) {
+            return state.cart_meals
+          },
 
+        loading(state) {
+            return state.loading
+        }
     },
     mutations: {
+        getCartMeals(state, cart_meals) {
+            state.cart_meals = cart_meals
+          },
 
+        setLoading(state, loading) {
+            state.loading = loading
+        },
     },
     actions: {
-
+        getCartMeals({ commit }) {
+            commit('setLoading', true)
+      
+            axiosInstance.get('/get-cart-meals')
+              .then(response => {
+                commit('getCartMeals', response.data.cart_meals)
+                commit('setLoading', false)
+              })
+              .catch(error => console.log(error))
+          },
     },
 }
 
