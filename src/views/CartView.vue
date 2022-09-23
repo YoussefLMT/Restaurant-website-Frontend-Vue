3c7b3005ffdda537424aac1dc30e88026d386e22
YoussefLMT <mcyoussef324@gmail.com>
2022-09-23 20:49:42 +0100

    add meal box

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -3,6 +3,38 @@
     <NavBar />
 </div>
 
+<div class="container mt-5">
+    <h3 class="text-center">Walcome in your cart</h3>
+    <div>
+        <!-- <div class="empty-cart mt-5">
+            <div class="row">
+                <div class="col-md-">
+                    <img src="@/assets/empty_cart.png">
+                    <h3>Your cart is currently empty</h3>
+                    <router-link to="/shop" class="btn btn-warning mt-3">Return To Shop</router-link>
+                </div>
+            </div>
+        </div> -->
+        <router-link to="/order" class="btn btn-primary">Order Now</router-link><br><br>
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-4">
+
+                <div class="box">
+                    <div class="imgbox">
+                        <img src="@/assets/pexels-pixabay-326279.jpg">
+                    </div>
+                    <div class="text">
+                        <h3>tt</h3>
+                        <p>66 DH</p>
+                        <!-- <router-link :to="{ name: 'mealDetails', params: {id: id }}" class="btn-show">Show</router-link> -->
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
 <Footer />
 </template>
 
