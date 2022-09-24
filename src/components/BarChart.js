e30cf96a0e4049f9bfadadcca236bbb94c800cc6
YoussefLMT <mcyoussef324@gmail.com>
2022-09-24 19:48:38 +0100

    add script

--- src/components/BarChart.js
+++
@@ -1,3 +0,0 @@
-<template>
-<Bar :chart-options="chartOptions" :chart-data="chartData" :chart-id="chartId" :dataset-id-key="datasetIdKey" :plugins="plugins" :css-classes="cssClasses" :styles="styles" :width="width" :height="height" />
-</template>
