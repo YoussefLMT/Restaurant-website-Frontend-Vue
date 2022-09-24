57bd9b4b5a76ea2bd667e2ca4fdb4a4c0eb37bc6
YoussefLMT <mcyoussef324@gmail.com>
2022-09-24 18:19:38 +0100

    add my orders card

--- src/views/MyOrdersView.vue
+++ src/views/MyOrdersView.vue
@@ -3,6 +3,26 @@
     <NavBar />
 </div>
 
+<div class="container mt-5">
+    <h3>My Orders</h3><br><br>
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="card mb-3" style="width: 18rem;">
+                <img class="card-img-top" src="@/assets/cart.png">
+                <div class="card-body">
+                    <h5 class="card-title">ttt</h5>
+                    <p class="card-text">300 DH</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 ml-5">
+            <p>Address: tt</p>
+            <p>City: tt</p>
+            <p class="badge text-bg-warning">Status: tt</p>
+        </div>
+    </div>
+</div>
+
 <Footer />
 </template>
 
