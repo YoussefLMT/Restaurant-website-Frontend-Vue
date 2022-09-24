96d4c24a3ec71270040f5e8d7b2b03cb25e21f43
YoussefLMT <mcyoussef324@gmail.com>
2022-09-24 17:25:45 +0100

    add order view

---
+++ src/views/OrderView.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="navbar">
+    <NavBar />
+</div>
+
+<Footer />
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+    components: {
+        NavBar,
+        Footer
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
