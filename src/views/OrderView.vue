96d1c34bb4a588a503b7d00945d928a211c205c0
YoussefLMT <mcyoussef324@gmail.com>
2022-09-24 18:08:36 +0100

    add total amount table

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -3,6 +3,30 @@
     <NavBar />
 </div>
 
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-6 mx-auto">
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <th scope="col">Amount</th>
+                        <td>100DH</td>
+                    </tr>
+                    <tr>
+                        <th scope="col">Shipment</th>
+                        <td>50DH</td>
+                    </tr>
+                    <tr>
+                        <th scope="col">Total Amount</th>
+                        <td>500DH</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    
+</div>
+
 <Footer />
 </template>
 
