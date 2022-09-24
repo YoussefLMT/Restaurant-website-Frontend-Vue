5e5e821a588bd885e2fb66adfc5f8a0ebdb08a45
YoussefLMT <mcyoussef324@gmail.com>
2022-09-24 18:23:04 +0100

    add user orders state, action, mutations and getters

--- src/store/OrdersModule.js
+++ src/store/OrdersModule.js
@@ -1,16 +1,40 @@
+import axiosInstance from '../axios'
+
 const OrdersModule = {
     namespaced: true,
     state: {
-
+        user_orders: [],
+        loading: false
     },
     getters: {
+        userOrders(state) {
+            return state.user_orders
+        },
 
+        loading(state) {
+            return state.loading
+        }
     },
     mutations: {
+        getUserOrders(state, user_orders) {
+            state.user_orders = user_orders
+        },
 
+        setLoading(state, loading) {
+            state.loading = loading
+        },
     },
     actions: {
+        getUserOrders({ commit }) {
+            commit('setLoading', true)
 
+            axiosInstance.get('/get-user-orders')
+                .then(response => {
+                    commit('getUserOrders', response.data.user_orders)
+                    commit('setLoading', false)
+                })
+                .catch(error => console.log(error))
+        },
     },
 }
 
