8d67b823fd4366a03afc9060cccb04440418f27c
YoussefLMT <mcyoussef324@gmail.com>
2022-09-24 18:39:17 +0100

    add orders view

---
+++ src/views/OrdersView.vue
@@ -0,0 +1,42 @@
+<template>
+<Sidebar />
+
+<div :style="{ 'margin-left': sidebarWidth }" class="orders">
+    <h1>Meals</h1>
+
+    <div class="card me" style="width: 1100px;">
+        <div class="card-header">
+            Orders Managment
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import Sidebar from '@/components/SideBar.vue'
+import {
+    sidebarWidth
+} from '@/components/sidebarState'
+
+export default {
+    components: {
+        Sidebar
+    },
+    data() {
+        return {
+            sidebarWidth,
+        }
+    },
+}
+</script>
+
+<style scoped>
+.orders {
+    padding: 20px;
+    transition: 0.3s ease;
+}
+
+.me {
+    margin: 50px auto;
+}
+</style>
