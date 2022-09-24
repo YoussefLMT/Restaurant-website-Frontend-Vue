1e237c45e0f9d6288904089c251513bee34b26e3
YoussefLMT <mcyoussef324@gmail.com>
2022-09-24 18:41:38 +0100

    add orders table

--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -8,6 +8,33 @@
         <div class="card-header">
             Orders Managment
         </div>
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">user id</th>
+                        <th scope="col">address</th>
+                        <th scope="col">city</th>
+                        <th scope="col">phone</th>
+                        <th scope="col">total amount</th>
+                        <th scope="col">status</th>
+                        <th scope="col">actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>6</td>
+                        <td>y</td>
+                        <td>y</td>
+                        <td>h</td>
+                        <td>y</td>
+                        <td class="badge text-bg-warning mt-2">yy</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 </template>
