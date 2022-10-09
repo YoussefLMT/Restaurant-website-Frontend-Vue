2eda317a3cdafdf5c6648597f80912f70f340fe1
YoussefLMT <mcyoussef324@gmail.com>
2022-10-09 18:27:38 +0100

    add script

--- src/components/DoughnutChart.vue
+++ src/components/DoughnutChart.vue
@@ -1,10 +1,40 @@
 <template>
-    <canvas id="myChart" width="400" height="400"></canvas>
+<canvas id="myChart" width="400" height="400"></canvas>
 </template>
 
 <script>
-export default {
+import Chart from 'chart.js/auto';
 
+export default {
+    mounted() {
+        const ctx = document.getElementById('myChart');
+
+        const data = {
+            labels: [
+                'Red',
+                'Blue',
+                'Yellow'
+            ],
+            datasets: [{
+                label: 'My First Dataset',
+                data: [300, 50, 100],
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)'
+                ],
+                hoverOffset: 4
+            }]
+        };
+
+        const myChart = new Chart(ctx, {
+            type: 'doughnut',
+            data: data,
+        });
+
+        myChart
+
+    },
 }
 </script>
 
